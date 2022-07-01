7f72b595421b5b5698bfdf249a20b04f739bca52
Loïc Dornel <creeperstone72.redstoner@gmail.com>
2022-07-01 18:16:45 +0200

    Poisson Disc Sampling
    Implemented the following features :
    + Poisson Disc Sampling
    + Scriptable Object to store sampling settings

---
+++ LevelGeneration/Assets/Features/PoissonDiscSampling/Scripts/MathUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Utility {
+    using static Mathf;
+    
+    public static class MathUtils {
+        public static readonly float Sqrt2 = Sqrt(2);
+
+        public static float RandomAngle => Random.value * PI * 2;
+
+        public static Vector2 AngleToDir(float angle) { return new Vector2(Sin(angle), Cos(angle)); }
+    }
+}
---
+++ LevelGeneration/Assets/Features/PoissonDiscSampling/Scripts/SamplingSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using ProceduralTerrainGeneration.Data;
+using UnityEngine;
+
+namespace PoissonDiscSampling {
+    [CreateAssetMenu(menuName = "Norsevar/Poisson Disc Sampling/Sampling Settings")]
+    public class SamplingSettings : UpdatableData {
+        public float radius = 1f;
+        public Vector2 regionCentre = Vector2.zero;
+        public Vector2 regionSize = Vector2.one;
+        public int rejectionSamples = 30;
+
+        public List<Vector2> SamplePoints() {
+            var points = PoissonDiscSampling.GeneratePoints(radius, regionSize, rejectionSamples);
+            for (var i = 0; i < points.Count; i++) points[i] -= regionSize / 2 - regionCentre;
+            return points;
+        }
+
+        #if UNITY_EDITOR
+        
+        protected override void OnValidate() {
+            base.OnValidate();
+            if (radius <= 0f) radius = 0.001f;
+            if (rejectionSamples < 1) rejectionSamples = 1;
+        }
+        
+        #endif
+    }
+}
