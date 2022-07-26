da73125b10d13557fd4d27d833b20e2e7e81e303
Loïc Dornel <creeperstone72.redstoner@gmail.com>
2022-07-26 15:58:16 +0200

    Procedural Tree Generation
    + Implemented Game Dev Guide's hexagon grid generator
    + Tweaked it to generate any regular polygon, then to build cones
    + Used the modified generator to create low-poly trees

---
+++ LevelGeneration/Assets/Features/ProceduralTreeGeneration/Scripts/TreeSettings.cs
@@ -0,0 +1,29 @@
+﻿namespace ProceduralTreeGeneration {
+    using UnityEngine;
+    using Utility;
+
+    [System.Serializable]
+    public class TreeSettings {
+        public Material material;
+        [Space(5)]
+        [SerializeField] private int minSideCount;
+        [SerializeField] private int maxSideCount;
+        [Space(5)]
+        [SerializeField] private float minBottomRadius;
+        [SerializeField] private float maxBottomRadius;
+        [Space(5)]
+        [SerializeField] private float minTopRadius;
+        [SerializeField] private float maxTopRadius;
+        [Space(5)]
+        [SerializeField] private float minHeight;
+        [SerializeField] private float maxHeight;
+
+        public int GetSideCount() { return RandUtils.RandInt(minSideCount, maxSideCount); }
+
+        public float GetBottomRadius() { return RandUtils.Rand(minBottomRadius, maxBottomRadius); }
+        
+        public float GetTopRadius() { return RandUtils.Rand(minTopRadius, maxTopRadius); }
+        
+        public float GetHeight() { return RandUtils.Rand(minHeight, maxHeight); }
+    }
+}
